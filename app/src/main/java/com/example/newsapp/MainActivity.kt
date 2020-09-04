bacbb6f15fd9a1d0d75484f4f306318111b05fb0
osualaFC <fredrick.osuala@decagon.dev>
2020-09-04 15:33:19 +0100

    setUp navigation components

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /**setUp navigation**/
+        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
     }
 }
