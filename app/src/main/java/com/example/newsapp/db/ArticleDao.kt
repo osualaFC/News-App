85733543f0801bf2e07ca651f2d8ad47f175feed
osualaFC <fredrick.osuala@decagon.dev>
2020-09-04 16:50:29 +0100

    setUp Room Entity and Dao

---
+++ app/src/main/java/com/example/newsapp/db/ArticleDao.kt
@@ -0,0 +1,24 @@
+package com.example.newsapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.newsapp.Article
+
+@Dao
+interface ArticleDao {
+
+    /**if an article already exist in the db..then replace**/
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    /**insert or update*/
+    suspend fun upsert(article: Article): Long
+
+    /**get all articles**/
+    @Query("SELECT * FROM articles")
+    /**i didnt use suspend fun becos suspend funs does not work with LiveData**/
+    fun getAllArticles(): LiveData<List<Article>>
+
+    /**delete article**/
+    @Delete
+    suspend fun deleteArticle(article: Article)
+
+}
